9c54fd1b55547d6483f0d0b5d6f84e9a193f736a
Elismar Silva <elismarsilva.5@outlook.com>
2025-04-22 20:28:09 -0300

    chore: i18n setup for multi language support

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import Header from './components/Header'
 import Sidebar from './components/Sidebar'
+import { useTranslation } from 'react-i18next'
 
 function App() {
+  const { t } = useTranslation()
+
   return (
     <div className="min-h-screen flex flex-col dark:bg-zinc-900 bg-white text-zinc-900 dark:text-zinc-100">
       <Header />
       <div className="flex flex-1 overflow-hidden">
         <Sidebar />
         <main className="flex-1 p-4 overflow-auto">
-          {/* Aqui entra o conteúdo de cada ferramenta */}
-          <h2 className="text-xl font-semibold">Bem-vindo à DevBox 👨‍💻</h2>
-          <p className="text-sm">Escolha uma ferramenta no menu</p>
+          <h2 className="text-xl font-semibold">{t('DevBox')}</h2>
+          <p className="text-sm">{t('ChooseTool')}</p>
         </main>
       </div>
     </div>
